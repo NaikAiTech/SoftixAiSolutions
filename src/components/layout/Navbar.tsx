7e75cef196dfb41457d08c895f3bc102ba0ea8ae
Talib Saleem Naik <65025576+TalibSaleemNaik@users.noreply.github.com>
2026-01-15 23:55:27 -0800

    Update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -54,7 +54,7 @@ export const Navbar = () => {
               whileTap={{ scale: 0.95 }}
               className="flex items-center gap-2"
             >
-              <div className="w-20 h-20  rounded-lg flex items-center justify-center text-primary-foreground font-bold text-xl shadow-lg">
+              <div className="w-20 h-20  rounded-lg flex items-center justify-center text-primary-foreground">
                <img src="./images/logo.png" />
               </div>
               
